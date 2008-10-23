234234fb353a4ec3d9277deb28d95cfc7d292052
Julian Stecklina <jsteckli@os.inf.tu-dresden.de>
2008-10-23 05:52:41 +0200

    Added MIT-like license.

--- packages.lisp
+++ packages.lisp
@@ -1,4 +1,7 @@
 ;;; -*- Mode: Lisp -*-
+;;; Copyright (c) 2008 Julian Stecklina
+;;; 
+;;; This file is part of CL-DBUS. Look into LICENSE for license terms.
 
 (defpackage :blitz.desktop.dbus
   (:use :common-lisp :iterate :defclass-star)
